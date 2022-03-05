b58b46d68027b6c9f6ff79769f57d03dd0f90627
Renan Lopes <905renan@gmail.com>
2022-03-05 14:25:10 -0300

    Procedural endless generation

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 using UnityEditor;
 
 [CustomEditor (typeof(MapGenerator))]
-public class MapGeneratorEditor : Editor
-{
-	public override void OnInspectorGUI()
-	{
+public class MapGeneratorEditor : Editor {
+	public override void OnInspectorGUI() {
 		MapGenerator mapGen = (MapGenerator)target;
 
-		if (DrawDefaultInspector())
-		{
-			if (mapGen.autoUpdate)
-			{
-				mapGen.GenerateMap();
+		if (DrawDefaultInspector()) {
+			if (mapGen.autoUpdate) {
+				mapGen.DrawMapInEditor();
 			}
 		}
 
-		if (GUILayout.Button("Generate"))
-		{
-			mapGen.GenerateMap();
+		if (GUILayout.Button("Generate")) {
+			mapGen.DrawMapInEditor();
 		}
 	}
 }
